335185b0736d61092e6f081f469a5f47add03398
ntfuko77 <ringochichidim@gmail.com>
2025-08-16 00:34:58 +0800

    Architecture flxes

--- bot_command.py
+++ bot_command.py
@@ -1,5 +1,12 @@
 import json
-def load(path='congifs/bot.json'):
-    ...
+def load(path=r'configs\bot.json'):
+    """Load bot configuration from a JSON file."""
+    with open(path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
 def ctreate_room():
     ...
+
+if __name__ == '__main__':
+    data=load()
+    print(data)
