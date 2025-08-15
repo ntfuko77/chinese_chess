3feb7242fc918c31bc0529c8e40b7f22b0e4985d
ntfuko77 <ringochichidim@gmail.com>
2025-08-16 00:34:32 +0800

    Architecture flix

--- inner_command.py
+++ inner_command.py
@@ -1,6 +1,8 @@
-
 import broad
+from asyncio import Lock
+class InnerCommand:
+    def __init__(self, command: str):...
 
-
+def parse_command(command: str):...
 #input:'馬一進二' or '車一平二'
 #output: {'type': 'move', 'piece': '馬', 'from': 1, 'to': 2}
