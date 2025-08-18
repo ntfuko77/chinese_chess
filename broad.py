34a8c714e59b84cb58f63e732bcd7614f97ec91a
ntfuko77 <ringochichidim@gmail.com>
2025-08-18 15:08:21 +0800

    readable

--- broad.py
+++ broad.py
@@ -1,16 +1,16 @@
 import numpy
 import json
 from PIL import Image
-from enum import Enum
 
-broad_size=(969,1118)
-class Layer(Enum):
-    broad_size=broad_size
-    enpty=Image.new("RGBA",broad_size,(0,0,0,0))
-class Base_piece(Enum):
+
+class Layer:
+    broad_size=(969,1118)
+    empty=Image.new("RGBA",broad_size,(0,0,0,0))
+class Base_piece:
     piece_space=[105,108]
     piece_fix=[17,24]
     piece_size=(90,90)
+
 class Broad():
     def __init__(self):
         self.layer=None
@@ -52,8 +52,8 @@ class Broad():
     @property
     def real_positon(self):
         out=self.basic_position.copy()
-        out=out*numpy.array(Base_piece.piece_space.value)
-        out=out+numpy.array(Base_piece.piece_fix.value)
+        out=out*numpy.array(Base_piece.piece_space)
+        out=out+numpy.array(Base_piece.piece_fix)
         return out
 class Picture_loader:
     def __init__(self):
@@ -63,10 +63,10 @@ class picture():
         self.loader=loader
         self.refresh()
         self.mask=Image.open("pic/mask.png")
-        self.mask=self.mask.resize(Base_piece.piece_size.value)
+        self.mask=self.mask.resize(Base_piece.piece_size)
         self.pieces=[Image.open('pic/b'+str(i)+'.png') for i in range(7)]
         self.pieces+=[Image.open('pic/r'+str(i)+'.png') for i in range(7)]
-        self.pieces=[i.resize(Base_piece.piece_size.value) for i in self.pieces]
+        self.pieces=[i.resize(Base_piece.piece_size) for i in self.pieces]
         return
     def draw_piece(self,broad:Broad):
         p_position=broad.real_positon
@@ -76,7 +76,7 @@ class picture():
             pic_num= broad.basic_state[number,0]
             pic = self.pieces[pic_num] if number <16 else self.pieces[pic_num+7]
             pos = p_position[number]
-            broad.layer = Layer.enpty.value.copy()
+            broad.layer = Layer.empty.copy()
             broad.layer.paste(pic, (int(pos[0]), int(pos[1])), self.mask)
         return
 
